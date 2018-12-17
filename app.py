b852cea8b7d54c1639d28d78630ff0764c2239ce
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-12-17 11:56:16 +0100

    allow connections from everywhere

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def index(file):
     return static_file(file, root=STATIC_FILES)
 
 def main():
-    run(host='localhost', port=8080, debug=True)
+    run(host='0.0.0.0', port=8080, debug=True)
 
 if __name__ == "__main__":
     main()
